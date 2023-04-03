5b273caf0e23450c0e06de5c89b59b466bdf0fee
myunbongs <myunbongs@naver.com>
2023-04-03 16:23:02 +0900

    ref: ♻️ 모델 이름 변경 및 seed list 수정 (#4)

--- app/image_generate.py
+++ app/image_generate.py
@@ -10,7 +10,7 @@ from .utils import image2string
 
 import io
 
-class Cover(BaseModel):
+class Txt2img(BaseModel):
     model: Optional[str]
     prompt: Optional[str] 
     negative_prompt: Optional[str] 
@@ -18,21 +18,16 @@ class Cover(BaseModel):
     width: Optional[int] 
     number_of_imgs: Optional[int] 
     imgs : Optional[str] = []
-    seed : Optional[int]
+    seeds : Optional[int] = []
 
-def make_book_cover(cover: Cover):
+def make_book_cover(txt2img: Txt2img):
 
-    if cover.model == "stable-diffusion":
-        model = "runwayml/stable-diffusion-v1-5"
-    elif cover.model == "anything":
-        model = "andite/anything-v4.0"
-    elif cover.model == "pastelmix":
-        model = "andite/pastel-mix"
-    else:
-        raise HTTPException(status_code=404, detail="Model not found")
+    model = select_model(txt2img.model)
 
-    seed = random.randint(1, 2147483647)
-    generator = torch.Generator("cuda").manual_seed(seed)
+    generator = torch.Generator(device="cuda")
+    seed = generator.seed()
+    txt2img.seeds.append(seed)
+    generator = generator.manual_seed(seed)
     
     pipe = StableDiffusionPipeline.from_pretrained(model, torch_dtype=torch.float16).to(
         "cuda"
@@ -41,15 +36,24 @@ def make_book_cover(cover: Cover):
     pipe.enable_xformers_memory_efficient_attention()
 
     with torch.inference_mode():
-        imgs = pipe(prompt=cover.prompt, negative_prompt=cover.negative_prompt, \
-                height=cover.height, width=cover.width, num_inference_steps=30, \
-                num_images_per_prompt=cover.number_of_imgs, guidance_scale=7.5, generator=generator).images        
+        imgs = pipe(prompt=txt2img.prompt, negative_prompt=txt2img.negative_prompt, \
+                height=txt2img.height, width=txt2img.width, num_inference_steps=30, \
+                num_images_per_prompt=txt2img.number_of_imgs, guidance_scale=7.5, generator=generator).images        
    
         for i, img in enumerate(imgs):
-            file_name = "./app/images/{}_{}_{}.png".format(cover.prompt, cover.seed, i)
+            file_name = "./app/images/{}_{}_{}.png".format(txt2img.prompt, generator.seed, i)
             img.save(file_name)
-            cover.imgs.append(image2string(img))
+            txt2img.imgs.append(image2string(img))
 
-        cover.seed = seed
+    return txt2img
 
-    return cover
+def select_model(model_name):
+    if model_name == "stable-diffusion":
+        model = "runwayml/stable-diffusion-v1-5"
+    elif model_name == "anything":
+        model = "andite/anything-v4.0"
+    elif model_name == "pastelmix":
+        model = "andite/pastel-mix"
+    else:
+        raise HTTPException(status_code=404, detail="Model not found")
+    return model 
--- app/main.py
+++ app/main.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
 
-from .image_generate import Cover, make_book_cover
+from .image_generate import Txt2img, make_book_cover
 
 from PIL import Image
 
 app = FastAPI()
 
 @app.post("/making-cover")
-async def making_cover(cover: Cover):
+async def making_cover(txt2img: Txt2img):
     global results
-    results = make_book_cover(cover)
+    results = make_book_cover(txt2img)
 
-    converted_results = jsonable_encoder(cover)
+    converted_results = jsonable_encoder(txt2img)
     return JSONResponse(content=converted_results)

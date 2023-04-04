172f977e2e5fb0e55a713377e2ad16b807ced893
myunbongs <myunbongs@naver.com>
2023-04-04 19:32:34 +0900

    fix: 🩹함수명 분리 (#4)

--- app/image_generate.py
+++ app/image_generate.py
@@ -5,6 +5,7 @@ from PIL import Image
 import random
 from typing import Optional
 from fastapi import HTTPException
+import os 
 
 from .utils import image2string
 
@@ -27,8 +28,6 @@ class Img2img(BaseModel):
     init_image_num: Optional[int]
     prompt: Optional[str] 
     negative_prompt: Optional[str] 
-    height: Optional[int] 
-    width: Optional[int] 
     num_inference_steps: Optional[int] = 30
     guidance_scale: Optional[int] = 7.5 
     strength: Optional[float] = 0.8
@@ -36,20 +35,17 @@ class Img2img(BaseModel):
     imgs : Optional[str] = []
     seeds : Optional[int] = []
 
-def txt2img(txt2img: Txt2img):
+def making_cover_stable_diffusion_txt2img(txt2img: Txt2img):
 
     model = select_model(txt2img.model)
 
-    generator = torch.Generator(device="cuda")
-    seed = generator.seed()
-    txt2img.seeds.append(seed)
-    generator = generator.manual_seed(seed)
-    
+    generator = [torch.Generator(device="cuda").manual_seed(0) for i in range(txt2img.number_of_imgs)]
+    img2img.seeds = generator
+
     pipe = StableDiffusionPipeline.from_pretrained(model, torch_dtype=torch.float16).to(
         "cuda"
     )
     
-    pipe.enable_vae_tiling()
     pipe.enable_xformers_memory_efficient_attention()
 
     with torch.inference_mode():
@@ -64,23 +60,25 @@ def txt2img(txt2img: Txt2img):
 
     return txt2img
 
-def img2img(img2img: Img2img):
+def making_cover_stable_diffusion_img2img(img2img: Img2img):
 
-    total_init_imgs_num = len(os.listdir('./app/init_image'))
-    init_img = select_model(img2img.init_image_num, total_init_imgs_num)
+    total_init_imgs_num = len(os.listdir('./app/init_images/'))
+    init_img = select_init_image(img2img.init_image_num, total_init_imgs_num)
 
     model = select_model(img2img.model)
-    
+
+    generator = [torch.Generator(device="cuda").manual_seed(0) for i in range(img2img.number_of_imgs)]
+    img2img.seeds = generator
+
     pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model, torch_dtype=torch.float16).to(
         "cuda"
     )
     
-    pipe.enable_vae_tiling()
     pipe.enable_xformers_memory_efficient_attention()
 
     with torch.inference_mode():
         imgs = pipe(prompt=img2img.prompt, negative_prompt=img2img.negative_prompt,
-                image=init_img, height=img2img.height, width=img2img.width, num_inference_steps=img2img.num_inference_steps, 
+                image=init_img, num_inference_steps=img2img.num_inference_steps, 
                 num_images_per_prompt=img2img.number_of_imgs, strength = img2img.strength, guidance_scale=img2img.guidance_scale, 
                 generator=generator).images   
    
@@ -103,8 +101,8 @@ def select_model(model_name):
         raise HTTPException(status_code=404, detail="Model not found")
     return model
 
-def select_model(init_image_num, total_num):
+def select_init_image(init_image_num, total_num):
     for i in range(total_num):
         if init_image_num == i:
-            init_image = Image.open('./app/init_images/{}.png'.format(i)).convert("RGB")
+            init_image = Image.open('./app/init_images/{}.jpg'.format(i)).convert("RGB")
     return init_image
--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
 
-from .image_generate import Txt2img, Img2img, txt2img, img2img
+from .image_generate import Txt2img, Img2img, making_cover_stable_diffusion_txt2img, making_cover_stable_diffusion_img2img
 
 from PIL import Image
 
@@ -12,15 +12,15 @@ app = FastAPI()
 @app.post("/txt2img")
 async def making_cover_txt2img(txt2img: Txt2img):
     global results
-    results = txt2img(txt2img)
+    results = making_cover_stable_diffusion_txt2img(txt2img)
 
-    converted_results = jsonable_encoder(txt2img)
+    converted_results = jsonable_encoder(results)
     return JSONResponse(content=converted_results)
 
 @app.post("/img2img")
 async def making_cover_img2img(img2img: Img2img):
     global results
-    results = img2img(img2img)
+    results = making_cover_stable_diffusion_img2img(img2img)
 
-    converted_results = jsonable_encoder(img2img)
+    converted_results = jsonable_encoder(results)
     return JSONResponse(content=converted_results)

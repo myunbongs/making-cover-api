b2d84d24af306517b4bb069c3b33b7acf90409f5
myunbongs <myunbongs@naver.com>
2023-04-08 21:39:47 +0900

    deploy: 🚀 cors error (#7)

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
-from typing import Union
 from fastapi import FastAPI
+
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+
 from starlette.responses import JSONResponse
 
 from .image_generate import Txt2img, Img2img, making_cover_stable_diffusion_txt2img, making_cover_stable_diffusion_img2img
 
 from PIL import Image
 
+origins = ["*"]
+
+origins = [
+    "https://makingcover.ai/",
+    "http://localhost:3333",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/txt2img")
 async def making_cover_txt2img(txt2img: Txt2img):
